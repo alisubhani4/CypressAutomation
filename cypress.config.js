92f9da274c37ff754d44af888a700f9d58d7cea2
Ali Subhani <ali.subhani@tintash.com>
2022-10-30 12:53:47 +0500

    Remove config file

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // Node event listeners can be implemented here if required
-    },
-  },
-});
